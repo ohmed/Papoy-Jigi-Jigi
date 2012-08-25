0e20d78d40b5f7906d3c9206cac033efa290b284
luspav <luspav@gmail.com>
2012-08-25 15:51:21 +0300

    parser & band finished

--- scripts/band.js
+++ scripts/band.js
@@ -1,23 +1,118 @@
 var band = band || {};
 
 band.play = function () {
+   var tempo = player.audio.duration / band.music.tactNum;
+  //var tempo = 186 / band.music.tactNum;
+  var self = this;
+  var i = 0;
+  var intervalID = setInterval(function () { 
+
+    band.guitar.play(band.music.guitar[i]);
+
+    band.bass.play(band.music.bass[i]);
+
+    band.vocals.play(band.music.vocals[i], tempo);
+    
+    i++; 
+
+    if (i >= band.music.tactNum) {
+      band.finish();
+      clearInterval(intervalID);
+    }
+
+  }, ( tempo ) * 1000 );
 
 };
 
 band.tune = function () {
+  console.log('tune');
+  var intervalID = setInterval(function () {
+    if (typeof band.music !== 'undefined') {
+      band.music.compose(band.play);
+      clearInterval(intervalID);
+    }
+  }, 500);
 };
 
 band.finish = function () {
+  console.log('finish');
 };
 
 band.vocals = {
+  play: function (chars, tempo) {
+    chars = chars || [];
+    if (chars.length === 0) return;
+    var self = this;
+    var j = 0;
+    var intervalID = setInterval(function () {
+      self._animateMouth[chars[j]]();
+      j++; 
+      if (chars[j] !== '0') {
+        m1.duration = (tempo / chars.length) * 1000;
+      }
+      if(j >= chars.length) 
+        clearInterval(intervalID);
+    }, (tempo / chars.length) * 1000);
+  },
+  _animateMouth: {
+    'a': function () {
+      console.log('a');
+      m1.play = 3;
+    },
+    'e': function () {
+      console.log('e');
+      m1.play = 3;
+    },
+    'i': function () {
+      console.log('i');
+      m1.play = 5;
+    },
+    'o': function () {
+      console.log('o');
+      m1.play = 2;
+    }, 
+    'u': function () {
+      console.log('u');
+      m1.play = 4;
+    },
+    'y': function () {
+      console.log('y');
+      m1.play = 4;//111111111
+    },
+    '-': function () {
+    }
+  }
 };
 
 band.drums = {
+  play: function () {
+
+  }
 };
 
 band.guitar = {
+  play: function (notes) {
+    /*console.log('guitar-play ' + notes);
+    var tactDuration = 1000 * player.audio.duration / band.music.tactNum;
+    m1.duration = Math.round( ( tactDuration / notes.length ) ) + 30;
+    var inter = 0;
+    for (var i = 0; i<notes.length; i++) {
+      if ( notes[i] !== 0 ) {
+        setTimeout( function() {
+          if (m1.play === 0) {
+            m1.play = 1;
+          }
+        }, inter);
+        inter += m1.duration + 30;
+      } else {
+        inter += m1.duration - 20;
+      }
+    }*/
+  }
 };
 
 band.bass = {
+  play: function () {
+    console.log('bass-play');
+  }
 };
--- scripts/parser.js
+++ scripts/parser.js
@@ -21,14 +21,127 @@
     'loaded': 0,
 
     _filePath: {
+      'ac_dc': '/resources/audio/ac_dc/',
+      '30_seconds_to_mars' : '/resources/audio/30_seconds_to_mars/'
     },
 
     load: function (song) {
+      song = song || 'ac_dc';
+      var self = this;
+      $.ajax({
+        url: self._filePath[song] + 'bass.txt',
+        success: function (data) {
+          self.bass = data.replace(/\r\n\r\n/g,'$')
+                            .replace(/\r\n/g,'%')
+                            .replace(/\$\%/g,'$')
+                            .replace(/\|\%/g,'%')
+                            .replace(/\|\$/g,'$');
+          self.loaded += 1;
+        }
+      });
+      $.ajax({
+        url: self._filePath[song] + 'guitar.txt',
+        success: function (data) {
+          self.guitar = data.replace(/\r\n\r\n/g,'$')
+                            .replace(/\r\n/g,'%')
+                            .replace(/\$\%/g,'$')
+                            .replace(/\|\%/g,'%')
+                            .replace(/\|\$/g,'$');
+          self.loaded += 1;
+        }
+      });
+      $.ajax({
+        url: self._filePath[song] + 'lyrics.txt',
+        success: function (data) {
+          self.lyrics = data;
+          self.loaded += 1;          
+          var lyricsTemp = self.lyrics;
+          self.lyrics = [];
+          for (var i = 0; i < lyricsTemp.length; i++) {
+            if (/[aeiouy]/.test(lyricsTemp[i])) {
+              self.lyrics.push(lyricsTemp[i]);
+            }
+          }
+        }
+      });
+      $.ajax({
+        url: self._filePath[song] + 'vocals.txt',
+        success: function (data) {
+          self.vocals = data.replace(/\r\n\r\n/g,'$')
+                            .replace(/\r\n/g,'%')
+                            .replace(/\$\%/g,'$')
+                            .replace(/\|\%/g,'%')
+                            .replace(/\|\$/g,'$');
+          self.loaded += 1;
+        }
+      });
+    }
   };
 
+  raws.load('30_seconds_to_mars');
+  // raws.load('ac_dc');
+
   music.compose = function (callback) {
+
+    var instruments = ['guitar', 'bass', 'vocals'];
+
+    for (var _i in instruments) {
+      var instrument = instruments[_i];
+      var parts = raws[ instrument ].split('$');
+      for (var i = 0; i < parts.length; i++) {
+        var strings = parts[i].split('%');
+        for (var j = 0; j < strings.length; j++) {
+          var _new = tabs[ instrument ][j].concat(strings[j].split('|'));
+          tabs[ instrument ][j] = _new;
+        }
+      }
+    }
+
+    music.tactNum = tabs.guitar[5].length;
+
+    for (var _i in instruments) {
+      var instrument = instruments[_i];
+      music[ instrument ] = tabs[ instrument ][0];
+    }
+
+    for (var _i in instruments) {
+      var instrument = instruments[_i];
+      for (var i = 0; i < music.bass.length - 1; i++) {
+        music[instrument][i] = music[instrument][i].split('');
+      }
+    }
+
+    for (var _i in instruments) {
+      var instrument = instruments[_i];
+      for (var i = 0; i < tabs[instrument].length; i++) {
+        for (var j = 0; j < music.bass.length - 1; j++) {
+          for (var k = 0; k < tabs[instrument][i][j].length; k++) {
+            if (music[ instrument ][j][k] === '-' && tabs[instrument][i][j][k] !== '-') {
+              music[ instrument ][j][k] = tabs[instrument][i][j][k];
+            }
+          }
+        }
+      }
+    }
+
+    for (var i = 0; i < music.vocals.length; i++) {
+      for (var j = 0; j < music.vocals[i].length; j++) {
+        if (music.vocals[i][j] !== '-') {
+          music.vocals[i][j] = raws.lyrics.pop() || 'o';
+        }
+      }
+    }
+
+    callback();
   }
 
   music.raws = raws;
+
+  var intervalID = setInterval(function () {
+    if (raws.loaded === 4) {
+      window.band.music = music;
+      clearInterval(intervalID);
+    }
+  }, 500);  
 
 }());
